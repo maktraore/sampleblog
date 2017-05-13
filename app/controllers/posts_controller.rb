44afca9fd499bd8a91d2a15263a9094951a0c3a6
Mamadou Traore <maktraore2000@gmail.com>
2017-05-13 00:28:21 -0400

    added edit, update, delete, added has_many actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "seyba", password: "12345", except:  [ :index, :show]
   def index
     @posts = Post.all
   end
@@ -7,15 +8,36 @@ class PostsController < ApplicationController
   end
     
   def new
+    @post = Post.new
   end
   def create
     # render plain: params[:post].inspect
     @post = Post.new(post_params)
-    @post.save
+    if @post.save
      redirect_to @post
+   else
+     render 'new'
+    end
   end
   private def post_params
-     params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body)
      
+  end
+  def edit
+      @post = Post.find(params[:id])
+
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+     redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root  'posts#index'
+  root  'posts#index', as: 'home'
 
-  resources :posts
+  resources :posts do 
+    resources :comments
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
